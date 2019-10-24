d337842b2aff43d4b8bf03de1790b34ce4871d3b
sehaj <sehajveerbri@gmail.com>
2019-10-24 15:01:05 -0600

    Changes to Inspector class
    Added the recursive ability to recurse and retrive the parent class if it exists.

--- Inspector.java
+++ Inspector.java
@@ -7,11 +7,26 @@ public class Inspector {
 
     private void inspectClass(Class c, Object obj, boolean recursive, int depth) {
     	System.out.println("Class name: " + c.getName());
-    	System.out.println("Super class name: " + c.getSuperclass().getName());
-    	
+    	System.out.print("Super classes in order from highest parent to child class: ");
+    	recurseSuperClass(c);
     	Class[] list = c.getInterfaces();
-    	
     }
+    
+    public void recurseSuperClass(Class<?> c) {
+		
+    	if(c.getSuperclass() == null) {
+    		if(c.getName().equals("java.lang.Object")) {
+    			return;
+    		}
+    		else {
+    			System.out.print (c.getName() + ", ");
+    			return;
+    		}
+    	}
+    	recurseSuperClass(c.getSuperclass());
+    		System.out.print(c.getName() + ", ");
+    }
+    
     
     public static void main (String [] args) {
     	ClassB a;
